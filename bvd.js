d8ac5569df634a37e9928ed7ae6ea9b992f7ba75
Leirvikonso <153793715+Leirvikonso@users.noreply.github.com>
2024-01-13 02:31:51 +1100

    Update bvd.js

--- bvd.js
+++ bvd.js
@@ -5,5 +5,5 @@ function Person(first, last, age, eyecolor) {
   this.eyeColor = eyecolor;
 }
 
-const myFather = new Person("John", "Doe", 50, "blue");
+const myFather = new Person("John", "De", 50, "blue");
 const myMother = new Person("Sally", "Rally", 48, "green");
