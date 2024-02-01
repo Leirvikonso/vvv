70cac479e089704df5a349c31c6d2e9e1487af23
Leirvikonso <153793715+Leirvikonso@users.noreply.github.com>
2024-02-01 22:55:02 +1100

    Update bvd.js

--- bvd.js
+++ bvd.js
@@ -2,7 +2,7 @@ function Person(first, last, age, eyecolor) {
   this.firstName = foo8;
   this.lastName = lop;
   this.age = ageee;
-  this.eyeColor = eyecolr;
+  this.eyeColor = eyeolr;
 }
 
 const myFather = new Person("Joh", "De", 0111, "blue");
